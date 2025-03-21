f35000106ad2c568adee1638283bb51dafbb0f11
QiuranQAQ <599906003@qq.com>
2025-03-21 23:58:38 +0800

    Site updated: 2025-03-21 23:58:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/hello-world/"];function toRandomPost(){
+var posts=["2025/03/21/hello-world/","2025/03/21/这是第一篇文章，标志着Hexo博客正式启用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
