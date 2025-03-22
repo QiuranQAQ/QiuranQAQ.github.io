462ccf9beba6e1b0711f9a0193f48c2eeb8415bc
QiuranQAQ <599906003@qq.com>
2025-03-22 18:56:03 +0800

    Site updated: 2025-03-22 18:56:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/22/GSG2025+资产/","2025/03/21/hello-world/","2025/03/22/IDM绿色版/","2025/03/21/这是第一篇文章，标志着Hexo博客正式启用/","2025/03/22/音乐解锁/"];function toRandomPost(){
+var posts=["2025/03/22/GSG2025+资产/","2025/03/22/IDM绿色版/","2025/03/21/这是第一篇文章，标志着Hexo博客正式启用/","2025/03/21/hello-world/","2025/03/22/音乐解锁/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
